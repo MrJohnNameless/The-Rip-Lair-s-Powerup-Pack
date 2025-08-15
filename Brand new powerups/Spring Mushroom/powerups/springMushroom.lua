a31d2c05e8913b7e24f93dcaf5d29dc1d44c82b4
deviousquacks23 <duchluong1603@gmail.com>
2025-08-15 19:44:24 +0800

    Update springMushroom.lua
    - Rotation is a lot smoother
    - Movement no longer uses data.moveSpeed, now regular p.speedX is used
    - You can now bounce off ceilings

--- Brand new powerups/Spring Mushroom/powerups/springMushroom.lua
+++ Brand new powerups/Spring Mushroom/powerups/springMushroom.lua
@@ -10,12 +10,13 @@
 	AwesomeZack - made the player sprites, edited by me
 	DaCulDood - made the powerup sprite 
 	
-	Version 1.0
+	Version 2.0
 	
 	NOTE: This requires customPowerups in order to work! Get it from the link above! ^^^
 ]]--
 
 local cp = require("customPowerups")
+local easing = require("ext/easing")
 
 local springMushroom = {}
 
@@ -89,15 +90,15 @@ local function canBounceAround(p)
     )
 end
 
-function springMushroom.onInitAPI()
-end
-
 function springMushroom.onEnable(p)	
 	p.data.springMushroom = {
 		rotation = 0,
-		scale = 1,
-		moveSpeed = 0,
+		rotEase = 0,
+		rotStart = 0,
 		occupiedRotation = false,
+
+		scale = 1,
+
 		oldDir = p.direction,
 		turnTimer = 0,
 		frame = 0
@@ -119,63 +120,59 @@ function springMushroom.onTickPowerup(p)
 		p:mem(0x162, FIELD_WORD, 2)
     	end
 
-	if p:mem(0x36,FIELD_BOOL) then
-		p:harm()
-		return
-	end
-
-	if p.mount ~= 0 then
-		p.keys.altJump = KEYS_DOWN
-	end
-
-	p.speedY = p.speedY - 0.15
-
-    	if GP then
-        	GP.preventPound(p)
-    	end
-
-    	if aw then
-        	aw.preventWallSlide(p)
-    	end
+	if p:mem(0x36,FIELD_BOOL) then p:harm() return end
 
     	p:mem(0x2C, FIELD_DFLOAT, 0)
     	p:mem(0x40, FIELD_WORD, 0)
         p:mem(0x3C,FIELD_BOOL, false)
 	p:mem(0x154,FIELD_WORD,-2)
 
-	if p.forcedState ~= 0 or p.inClearPipe then data.moveSpeed = 0 end
+	if p.mount ~= 0 then p.keys.altJump = KEYS_DOWN end
+
+	p.speedX = math.clamp(p.speedX, -3, 3)
+	p.speedY = p.speedY - 0.15
+
+    	if GP then GP.preventPound(p) end
+	if aw then aw.preventWallSlide(p) end
 
    	if not canBounceAround(p) then return end
 
-	p.speedX = data.moveSpeed
-	data.moveSpeed = math.clamp(data.moveSpeed, -3, 3)
-
 	if p.keys.left then
 		p.direction = -1
-		data.moveSpeed = data.moveSpeed - 0.05
-
+		p.speedX = p.speedX + 0.05
 	elseif p.keys.right then
 		p.direction = 1
-		data.moveSpeed = data.moveSpeed + 0.05
+		p.speedX = p.speedX - 0.05
 	end
 
-	if isOnGround(p) then
+	if isOnGround(p) or p:mem(0x14A,FIELD_WORD) > 0 then
 		if p.keys.jump == KEYS_DOWN then
-			p.speedY = -16
+			if p:mem(0x14A,FIELD_WORD) > 0 then
+				p.speedY = 10
+			else
+				p.speedY = -16
+			end
+
 			SFX.play("powerups/bigBounce.ogg")
 			data.scale = 2
 		else
-			p.speedY = -8
+			if p:mem(0x14A,FIELD_WORD) > 0 then
+				p.speedY = 5
+			else
+				p.speedY = -8
+			end
+
 			SFX.play("powerups/smallBounce.ogg")
 			data.scale = 0.15
 
 			-- slinky!!
 			for k, v in Block.iterateIntersecting(p.x + 2, p.y + (p.height - 2), p.x + (p.width - 2), p.y + (p.height + 16)) do
-            			if p:mem(0x48,FIELD_WORD) ~= 0 and (Block.SLOPE_MAP[v.id] and Block.config[v.id].floorslope ~= 0 and not v.isHidden and not v:mem(0x5A, FIELD_BOOL)) then
+            			if p:mem(0x14A,FIELD_WORD) == 0 and p:mem(0x48,FIELD_WORD) ~= 0 and
+				(Block.SLOPE_MAP[v.id] and Block.config[v.id].floorslope ~= 0 and not v.isHidden and not v:mem(0x5A, FIELD_BOOL)) then
 					if Block.SLOPE_LR_FLOOR_MAP[v.id] and Block.config[v.id].floorslope == -1 then
-						data.moveSpeed = data.moveSpeed + 0.5
+						p.speedX = p.speedX + 0.5
 					elseif Block.SLOPE_RL_FLOOR_MAP[v.id] and Block.config[v.id].floorslope == 1 then
-						data.moveSpeed = data.moveSpeed - 0.5
+						p.speedX = p.speedX - 0.5
 					end
             			end
         		end
@@ -188,17 +185,21 @@ function springMushroom.onTickPowerup(p)
 	-- Wall bouncing
 
 	if p:mem(0x148, FIELD_WORD) == 2 then 
-		data.moveSpeed = 3 
+		p.speedX = 3 
 		p.speedY = -6
+
 		SFX.play("powerups/smallBounce.ogg")
+
 		data.scale = 0.15
 		data.occupiedRotation = true
 	end
 
 	if p:mem(0x14C, FIELD_WORD) == 2 then 
-		data.moveSpeed = -3 
+		p.speedX = -3 
 		p.speedY = -6
+
 		SFX.play("powerups/smallBounce.ogg")
+
 		data.scale = 0.15
 		data.occupiedRotation = true
 	end
@@ -209,10 +210,15 @@ function springMushroom.onTickEndPowerup(p)
 	local data = p.data.springMushroom
 
 	if data.occupiedRotation then
-		data.rotation = data.rotation + 5 * p.direction
+		data.rotEase = math.min(data.rotEase + 0.025, 1)
+		data.rotation = easing.outCubic(data.rotEase, data.rotStart, 180 * p.direction, 1)
+
 		if (data.rotation % 180) == 0 then
 			data.occupiedRotation = false 
 		end
+	else
+		data.rotEase = 0
+		data.rotStart = data.rotation
 	end
 
         if data.scale > 1 then 
@@ -237,12 +243,22 @@ function springMushroom.onTickEndPowerup(p)
 	end
 end
 
+local function canDraw(p)
+    	return (
+        	p.deathTimer == 0 and not p:mem(0x13C,FIELD_BOOL) -- Dead
+        	and not ({5, 8, 10})[p.forcedState]               -- In a forced state that prevents rendering
+		and not p:mem(0x142,FIELD_BOOL)                   -- Flashing
+        	and not p:mem(0x0C,FIELD_BOOL)                    -- Fairy
+    	)
+end
+
 function springMushroom.onDrawPowerup(p)
 	if not p.data.springMushroom then return end
 	local data = p.data.springMushroom
 
-	if p.deathTimer == 0 then 
+	if canDraw(p) then 
 		p:setFrame(-50 * p.direction)	
+
 		Graphics.drawBox{
 			texture = springMushroom:getAsset(p.character, springMushroom.playerImages[p.character]),
 			priority = (p.forcedState == 3 and -70) or -25,
